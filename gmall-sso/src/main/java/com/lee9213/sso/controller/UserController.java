d97c2b9b2abb4f2c58e5be8b8d3232b063d05157
lee9213 <lee9213@163.com>
2018-08-25 11:06:06 +0800

    add user register

--- gmall-sso/src/main/java/com/lee9213/sso/controller/UserController.java
+++ gmall-sso/src/main/java/com/lee9213/sso/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.lee9213.sso.controller;
 
 import com.lee9213.common.annotation.Anoymous;
+import com.lee9213.sso.enums.ResponseEnum;
 import com.lee9213.sso.support.ResponseData;
 import com.lee9213.sso.support.ServletHolder;
+import com.lee9213.user.constant.KafkaTopicConstant;
 import com.lee9213.user.dto.UserLoginRequest;
 import com.lee9213.user.dto.UserLoginResponse;
+import com.lee9213.user.dto.UserRegisterRequest;
+import com.lee9213.user.dto.UserRegisterResponse;
 import com.lee9213.user.service.IUserCoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +25,9 @@ public class UserController extends BaseController {
 
     @Autowired
     private IUserCoreService userCoreService;
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
 
     @Anoymous
     @PostMapping("/login")
@@ -41,8 +48,26 @@ public class UserController extends BaseController {
         return data;
     }
 
-    @GetMapping("/test")
-    public String test() {
-        return "success:" + getUid();
+    @Anoymous
+    @PostMapping("/register")
+    public ResponseData register(String username, String password, String mobile) {
+        ResponseData data = new ResponseData();
+
+        UserRegisterRequest request = new UserRegisterRequest();
+        request.setMobile(mobile);
+        request.setUsername(username);
+        request.setPassword(password);
+        try {
+            UserRegisterResponse response = userCoreService.register(request);
+            //异步化解耦
+            kafkaTemplate.send(KafkaTopicConstant.LUCKY_DRAW_CHANCE_TOPIC, response.getUid().toString());
+
+            data.setMsg(response.getMsg());
+            data.setCode(response.getCode());
+        } catch (Exception e) {
+            data.setMsg(ResponseEnum.FAILED.getMsg());
+            data.setCode(ResponseEnum.FAILED.getCode());
+        }
+        return data;
     }
 }
